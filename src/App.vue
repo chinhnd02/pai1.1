cb4e1819303e9f20683f5f7ae9fbc4562f4293e9
DucChinh <ducchinhcp882002@gmail.com>
2024-01-24 11:18:53 +0700

    bai1.1

---
+++ src/App.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  /* background-image: ; */
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-image: url("D:\Tokyo_Tech_Lab\btap\bai1\bai1\src\assets\Group.png");
+}
+
+.title {
+  text-align: center;
+  padding: 35px 0 55px 0;
+  font-family: "Epilogue", sans-serif;
+  font-weight: 700;
+  font-size: 56px;
+  line-height: 64px;
+  color: #000000;
+}
+
+.grid-container {
+  display: grid;
+  justify-content: center;
+  grid-template-columns: 493px 493px 493px;
+  /*Make the grid smaller than the container*/
+  gap: 20px;
+  /* background-color: #2196F3; */
+  padding: 10px;
+}
+
+.grid-container > div {
+  background-color: rgba(255, 255, 255, 0.8);
+  text-align: center;
+  padding: 20px 0;
+  font-size: 30px;
+  height: 200px;
+  border-radius: 16px;
+}
+
+.element {
+  display: grid;
+  justify-items: start;
+}
+
+.img-element {
+  width: 30px;
+  height: 30px;
+  margin-top: 20px;
+  margin-left: 39px;
+  margin-bottom: 20px;
+}
+
+.text-large {
+  font-family: "Epilogue", sans-serif;
+  font-size: 24px;
+  font-weight: 400;
+  line-height: 24.6px;
+  color: #333333;
+  margin-left: 39px;
+  margin-bottom: 10px;
+}
+
+.text-small {
+  font-family: "Epilogue", sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 21px;
+  color: #555555;
+  margin-left: 37px;
+  text-align: left;
+  margin-right: 58px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="hello">
+    <h1 class="title">Inside Open Art</h1>
+
+    <div class="grid-container">
+      <div class="element">
+        <img class="img-element" src="../assets/Prison.png" alt="" />
+        <p class="text-large">4pt gird system</p>
+        <p class="text-small">
+          Base on 4pt grid system. Our UI kit help you create perfect white
+          space
+        </p>
+      </div>
+      <div class="element">
+        <img class="img-element" src="../assets/Color Dropper.png" alt="" />
+        <p class="text-large">Color style</p>
+        <p class="text-small">
+          All color in OpenArt are styled. You can change all design color with
+          one click
+        </p>
+      </div>
+      <div class="element">
+        <img class="img-element" src="../assets/Text Color.png" alt="" />
+        <p class="text-large">Free font</p>
+        <p class="text-small">
+          OpenArt use Epilogue font family Available with open licence in
+          gooogle font
+        </p>
+      </div>
+      <div class="element">
+        <img class="img-element" src="../assets/Toggle On.png" alt="" />
+        <p class="text-large">Darkmode avaiable</p>
+        <p class="text-small">
+          Our UI Kit support darkmode Chage your design to dark mode with one
+          click
+        </p>
+      </div>
+      <div class="element">
+        <img class="img-element" src="../assets/Design.png" alt="" />
+        <p class="text-large">Easy to customize</p>
+        <p class="text-small">Create any design with OpenArt UI kits</p>
+      </div>
+      <div class="element">
+        <img class="img-element" src="../assets/Four Squares.png" alt="" />
+        <p class="text-large">Variant components</p>
+        <p class="text-small">
+          All component art variant, easy to design, easy to control
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
